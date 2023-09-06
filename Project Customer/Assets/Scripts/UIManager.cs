8ca64033e0d3bbbcb8b2baf3edfa0b99ff87c92a
DariaCristiana24 <dariacristiana24@gmail.com>
2023-09-06 16:44:03 +0200

    added objecct description

--- Project Customer/Assets/Scripts/UIManager.cs
+++ Project Customer/Assets/Scripts/UIManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -6,7 +7,18 @@ using UnityEngine;
 public class UIManager : MonoBehaviour
 {
     public TextMeshProUGUI ItemName;
+    public TextMeshProUGUI ItemDescription;
     public TextMeshProUGUI OxygenLeftText;
+
+    [Serializable]
+    public struct ObjectNamesAndDescriptions
+    {
+         public string Name;
+         public string Description;
+    }
+
+    public ObjectNamesAndDescriptions[] Objects;
+
 
     private PhysicsPickup player;
 
@@ -15,8 +27,11 @@ public class UIManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        ItemDescription.text = null;
+        
         player = FindObjectOfType<PhysicsPickup>();
         ItemName.text = player.objectName;
+        
 
         OxygenLeftText.text = "Oxygen: " + Oxygen;
     }
@@ -27,8 +42,53 @@ public class UIManager : MonoBehaviour
         if(ItemName.text != player.objectName)
         {
             ItemName.text = player.objectName;
+            descriptionCheck();
+            //change desc
         }
         oxygenRundown();
+    }
+
+    void descriptionCheck()
+    {
+        if (ItemName.text != null)
+        {
+            ItemDescription.text = findDesc(ItemName.text);
+
+            /*if (ItemName.text == "Bucket")
+            {
+                //ItemDescription.text = "maybe i can fill this with water";
+                ItemDescription.text = Objects[0].Description;
+                
+            }
+            if (ItemName.text == "Blanket")
+            {
+                //ItemDescription.text = "maybe i can wet thiss";
+            }
+            if (ItemName.text == "Key")
+            {
+                //ItemDescription.text = "this a key";
+            }
+            if (ItemName.text == "Fire Extinguisher")
+            {
+                //ItemDescription.text = "ur moms favorite fire extinguisher";
+            }*/
+        }
+        else
+        {
+            ItemDescription.text = null;
+        }
+    }
+
+    string findDesc(string objectToLookFor)
+    {
+        foreach(var o in Objects)
+        {
+            if(o.Name == objectToLookFor.ToString())
+            {
+                return o.Description;
+            }
+        }
+        return null;
     }
     void oxygenRundown()
     {
