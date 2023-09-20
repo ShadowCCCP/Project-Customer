b7964f75bb87dacb642cd3441a43e28fb52e45c9
DariaCristiana24 <dariacristiana24@gmail.com>
2023-09-20 10:11:17 +0200

    added volume slider with player pref

--- Project Customer/Assets/Scripts/UI/VolumeSlider.cs
+++ Project Customer/Assets/Scripts/UI/VolumeSlider.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public class VolumeSlider : MonoBehaviour
@@ -15,12 +16,18 @@ public class VolumeSlider : MonoBehaviour
     void Start()
     {
         audioManager = FindObjectOfType<AudioManager>();
+        audioManager.ChangeVolume(PlayerPrefs.GetFloat("volume"));
+        volumeSlider.value = PlayerPrefs.GetFloat("volume");
+
     }
 
     // Called when the slider value changes
     void Update()
     {
         audioManager.ChangeVolume(volumeSlider.value);
+        PlayerPrefs.SetFloat("volume", volumeSlider.value);
+
+
     }
 
 }
